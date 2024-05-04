f9d6fe73f77b6f6a11c72e1315d49a4991aa6324
rahogata <shivakumarkr1@gmail.com>
2024-05-04 21:36:45 +0530

    add counter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function countDays(): string {
+    const targetDate: Date = new Date('2024-09-15');
+    const currentDate: Date = new Date();
+    let months: number = targetDate.getMonth() - currentDate.getMonth();
+    let days: number = targetDate.getDate() - currentDate.getDate();
+
+    let result: string = "";
+    if (months > 0) {
+        result += `${months} month${months > 1 ? 's' : ''} `;
+    }
+    if (days > 0) {
+        result += `${days} day${days > 1 ? 's' : ''}`;
+    }
+
+    return result.trim();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{countDays()} Left</p>
       </header>
     </div>
   );
