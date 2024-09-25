99a789e19640729f5867247c34994e826456bb89
rahogata <shivakumarkr@rahogata.co.in>
2024-09-26 01:32:11 +0530

    auto increment months

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import axios from 'axios';
+import { DefaultApi } from 'finnhub-ts'
 import './App.css';
 
 function App() {
 
   const [price, setPrice] = useState('');
-  const finalDate = '2024-09-19'
+  const finalDate = '2024-09-17';
 
   useEffect(() => {
     const fetchStockPrice = async () => {
       try {
-        const apiKey = 'K19XJL3QDECB6T14';
-        const symbol = 'ZS'; // Zscaler stock symbol
-        const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`;
-        
-        const response = await axios.get(url);
-        const stockData = response.data['Global Quote'];
-        const price = parseFloat(stockData['04. low']).toFixed(2);
-        
-        setPrice(price);
+        const finnhubClient = new DefaultApi({
+          apiKey: 'crq4s6hr01qutsn3dfh0crq4s6hr01qutsn3dfhg',
+          isJsonMime: (input) => {
+            try {
+              JSON.parse(input)
+              return true
+            } catch (error) {}
+            return false
+          },
+        })
+        finnhubClient.quote('ZS').then((res) => {
+          setPrice(res.data.c?.toFixed(2) || '');
+        }, (err) => console.error('Error fetching stock price:', err));
       } catch (error) {
         console.error('Error fetching stock price:', error);
       }
@@ -28,8 +33,28 @@ function App() {
     fetchStockPrice();
   }, []);
 
+  function getMonthsBetweenDates(date1: Date, date2: Date) {
+    let year1 = date1.getFullYear();
+    let year2 = date2.getFullYear();
+    let month1 = date1.getMonth();
+    let month2 = date2.getMonth();
+    let yearDiff = year2 - year1;
+    let monthDiff = month2 - month1;
+    let totalMonths = yearDiff * 12 + monthDiff;
+    if (date2.getDate() < date1.getDate()) {
+      totalMonths -= 1;
+    }
+    return totalMonths;
+  }
+
+  function getTargetDate() {
+    const next3 = new Date(finalDate);
+    next3.setMonth(next3.getMonth() + 3 * ((getMonthsBetweenDates(next3, new Date()) / 3) + 1));
+    return next3;
+  }
+
   function countTimeLeft(): string {
-    const targetDate: Date = new Date(finalDate);
+    const targetDate: Date = getTargetDate();
     const currentDate: Date = new Date();
     let months: number = targetDate.getMonth() - currentDate.getMonth();
     let days: number = targetDate.getDate() - currentDate.getDate();
@@ -57,7 +82,7 @@ function App() {
   }
 
   function countDays(): number {
-    const targetDate: Date = new Date(finalDate);
+    const targetDate: Date = getTargetDate();
     const currentDate: Date = new Date();
     currentDate.setHours(0, 0, 0, 0);
     targetDate.setHours(0, 0, 0, 0);
