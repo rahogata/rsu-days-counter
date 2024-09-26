defddcd6dcd0901439c27b582bd9fcc3f41e833f
rahogata <shivakumarkr@rahogata.co.in>
2024-09-26 14:31:16 +0530

    automatic next 3 months rollout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
-import axios from 'axios';
 import { DefaultApi } from 'finnhub-ts'
 import './App.css';
 
@@ -33,7 +32,7 @@ function App() {
     fetchStockPrice();
   }, []);
 
-  function getMonthsBetweenDates(date1: Date, date2: Date) {
+  function getMonthsBetweenDates(date1: Date, date2: Date): number {
     let year1 = date1.getFullYear();
     let year2 = date2.getFullYear();
     let month1 = date1.getMonth();
@@ -47,26 +46,29 @@ function App() {
     return totalMonths;
   }
 
+  function getCurrentDate() {
+    return new Date();
+  }
+
   function getTargetDate() {
     const next3 = new Date(finalDate);
-    next3.setMonth(next3.getMonth() + 3 * ((getMonthsBetweenDates(next3, new Date()) / 3) + 1));
+    next3.setMonth(next3.getMonth() + 3 * (((getMonthsBetweenDates(next3, getCurrentDate()) / 3) | 0) + 1));
     return next3;
   }
 
   function countTimeLeft(): string {
     const targetDate: Date = getTargetDate();
-    const currentDate: Date = new Date();
-    let months: number = targetDate.getMonth() - currentDate.getMonth();
+    const currentDate: Date = getCurrentDate();
+    let months: number = getMonthsBetweenDates(currentDate, targetDate);
     let days: number = targetDate.getDate() - currentDate.getDate();
 
     if (days < 0) {
         const prevMonthLastDay = new Date(targetDate.getFullYear(), targetDate.getMonth(), 0).getDate();
         days += prevMonthLastDay;
-        months--;
     }
 
     let result: string = "";
-    if (months > 0) {
+    if (months < 3) {
         result = `${months} month${months > 1 ? 's' : ''} `;
     }
     if (days > 0) {
@@ -81,15 +83,15 @@ function App() {
     return result.trim();
   }
 
-  function countDays(): number {
+  function countDays(): string {
     const targetDate: Date = getTargetDate();
-    const currentDate: Date = new Date();
+    const currentDate: Date = getCurrentDate();
     currentDate.setHours(0, 0, 0, 0);
     targetDate.setHours(0, 0, 0, 0);
     const differenceMs = targetDate.getTime() - currentDate.getTime();
 
     const daysLeft = Math.ceil(differenceMs / (1000 * 60 * 60 * 24));
-    return daysLeft;
+    return daysLeft < 90 ? `${daysLeft}` : '';
   }
 
   return (
